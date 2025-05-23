c32bd4dc2a9fbf517d2722381f589de74d944405
8514a <ann8514a@gmail.com>
2025-05-23 17:14:10 +0300

    Modificări la page.tsx și next.config.ts

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,19 @@ import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@/components";
 
 
 
-export default async function Home({ searchParams }: HomeProps) {
-  const allCars = await fetchCars({
+export default async function Home(props: HomeProps) {
+  const searchParams = await props.searchParams;
+  const allCars = await fetchCars 
+ 
+({
     manufacturer: searchParams.manufacturer || "",
     year: searchParams.year || 2022,
     fuel: searchParams.fuel || "",
     limit: searchParams.limit || 10,
     model: searchParams.model || "",
-  });
+  }); 
+
+
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
